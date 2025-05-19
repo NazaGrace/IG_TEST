e598ffbc5d52a153ec5ef98469c381b1e5bc6f59
Ezuma Grace <nazagrace2952@gmail.com>
2025-05-19 12:21:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,17 @@ app.add_middleware(
 def root():
     return {"response": "Active!"}
 
+const VERIFY_TOKEN = "igcallback_token";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("WEBHOOK_VERIFIED");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
